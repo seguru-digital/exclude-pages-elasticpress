d0022e9e93b9b5c4a2694c51c0e08681e95394ae
Cameron Gilroy <git@camerongilroy.com>
2024-07-07 17:50:37 +1000

    Seguru Updates

--- exclude-pages-elasticpress.php
+++ exclude-pages-elasticpress.php
@@ -5,7 +5,7 @@
   * Description:       Allows users to select pages that should not be indexed by ElasticPress.
   * Requires at least: 6.3.0
   * Requires PHP:      7.4
-  * Version:           0.0.1
+  * Version:           0.0.2
   * Author:            seguru-digital
   * License:           GPL-2.0-or-later
   * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -22,7 +22,7 @@
  define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
  define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
  define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
- define($plugin_prefix . '_VER', '0.0.1');
+ define($plugin_prefix . '_VER', '0.0.2');
  define($plugin_prefix . '_CACHE_KEY', 'exclude_pages_from_elasticpress-cache-key-for-plugin');
  define($plugin_prefix . '_REMOTE_URL', 'https://plugins.seguru.xyz/wp-content/uploads/downloads/12/info.json');
  
@@ -37,41 +37,41 @@
  );
 
 // Hook to add meta box
-add_action('add_meta_boxes', 'epep_add_meta_box');
+add_action('add_meta_boxes', 'seguru_add_meta_box');
 
 // Hook to save meta box data
-add_action('save_post', 'epep_save_meta_box_data');
+add_action('save_post', 'seguru_save_meta_box_data');
 
 // Hook to filter ElasticPress indexing
-add_filter('ep_post_sync_args_post_prepare_meta', 'epep_exclude_pages_from_indexing', 10, 2);
+add_filter('ep_post_sync_args_post_prepare_meta', 'seguru_exclude_pages_from_indexing', 10, 2);
 
-function epep_add_meta_box() {
+function seguru_add_meta_box() {
     add_meta_box(
-        'epep_meta_box', // ID
+        'seguru_meta_box', // ID
         'ElasticPress Exclude', // Title
-        'epep_meta_box_callback', // Callback
+        'seguru_meta_box_callback', // Callback
         'page', // Post type
         'side' // Context
     );
 }
 
-function epep_meta_box_callback($post) {
-    wp_nonce_field('epep_save_meta_box_data', 'epep_meta_box_nonce');
+function seguru_meta_box_callback($post) {
+    wp_nonce_field('seguru_save_meta_box_data', 'seguru_meta_box_nonce');
 
-    $value = get_post_meta($post->ID, '_epep_exclude', true);
+    $value = get_post_meta($post->ID, '_seguru_exclude', true);
 
-    echo '<label for="epep_exclude">';
+    echo '<label for="seguru_exclude">';
     echo 'Exclude this page from ElasticPress indexing';
     echo '</label> ';
-    echo '<input type="checkbox" id="epep_exclude" name="epep_exclude" value="1" ' . checked($value, '1', false) . ' />';
+    echo '<input type="checkbox" id="seguru_exclude" name="seguru_exclude" value="1" ' . checked($value, '1', false) . ' />';
 }
 
-function epep_save_meta_box_data($post_id) {
-    if (!isset($_POST['epep_meta_box_nonce'])) {
+function seguru_save_meta_box_data($post_id) {
+    if (!isset($_POST['seguru_meta_box_nonce'])) {
         return;
     }
 
-    if (!wp_verify_nonce($_POST['epep_meta_box_nonce'], 'epep_save_meta_box_data')) {
+    if (!wp_verify_nonce($_POST['seguru_meta_box_nonce'], 'seguru_save_meta_box_data')) {
         return;
     }
 
@@ -89,17 +89,17 @@ function epep_save_meta_box_data($post_id) {
         }
     }
 
-    if (!isset($_POST['epep_exclude'])) {
+    if (!isset($_POST['seguru_exclude'])) {
         return;
     }
 
-    $exclude = sanitize_text_field($_POST['epep_exclude']);
+    $exclude = sanitize_text_field($_POST['seguru_exclude']);
 
-    update_post_meta($post_id, '_epep_exclude', $exclude);
+    update_post_meta($post_id, '_seguru_exclude', $exclude);
 }
 
-function epep_exclude_pages_from_indexing($prepared_post, $post) {
-    $exclude = get_post_meta($post->ID, '_epep_exclude', true);
+function seguru_exclude_pages_from_indexing($prepared_post, $post) {
+    $exclude = get_post_meta($post->ID, '_seguru_exclude', true);
 
     if ($exclude == '1') {
         return false; // Prevent indexing
